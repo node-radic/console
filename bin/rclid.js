0bfa315a33ff58f957df0d4989d071e95e850878
Robin Radic <robin@radic.nl>
2016-12-21 09:12:54 +0100

    upd

---
+++ bin/rclid.js
@@ -0,0 +1,18 @@
+"use strict";
+const dgram = require('dgram');
+const server = dgram.createSocket('udp4');
+function parseMessage(msg) {
+}
+server.on('error', (err) => {
+    console.log(`server error:\n${err.stack}`);
+    server.close();
+});
+server.on('message', (msg, rinfo) => {
+    console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
+});
+server.on('listening', () => {
+    var address = server.address();
+    console.log(`server listening ${address.address}:${address.port}`);
+});
+server.bind(41234);
+//# sourceMappingURL=rclid.js.map
