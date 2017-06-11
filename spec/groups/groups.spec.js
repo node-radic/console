8f131f5d07e1160fc7cb1251711a9919586f5820
Robin Radic <robin@radic.nl>
2017-06-11 03:17:57 +0200

    build publish

--- spec/groups/groups.spec.js
+++
@@ -1,6 +0,0 @@
-describe("groups", function () {
-    describe('Check for awesome', function () {
-        it('should be awesome', function () { return expect(true).toBeTruthy(); });
-    });
-});
-//# sourceMappingURL=groups.spec.js.map
